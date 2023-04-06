c55b23629790aed7b4f02c1aedcdd912cf23c222
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-04-06 11:39:59 -0400

    update items

--- SimulationUI/Assets/Scripts/GameManager.cs
+++ SimulationUI/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class GameManager : MonoBehaviour
 {
     public Animator BuymesgAnim;
@@ -11,7 +12,14 @@ public class GameManager : MonoBehaviour
     private string buyItem;
     public TMP_Text buymesgText;
     public TMP_Text ListText;
+    public TMP_Text LmilkText;
+    public TMP_Text SmilkText;
     public GameObject ListPanel;
+    public int Lmilks = 0;
+    public int Smilks = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,22 +33,33 @@ public class GameManager : MonoBehaviour
         //if press L key, buy large milk
         if (Input.GetKeyDown(KeyCode.L))
         {
+            ListText.text = "Items: ";
             buyItem = "large milk(Purple)";
             buymesgText.text = "You bought " + buyItem + " !";
+
+            Lmilks += 1;
+
             //update list text
-            ListText.text = buyItem;
+            LmilkText.text = buyItem + " " + Lmilks;
+
             //play animation
             BuymesgAnim.SetTrigger("buyUp");
             Invoke("buyReminderOff", 3.5f);
 
         }
+
+
+
         //if press S key, buy small milk
         if (Input.GetKeyDown(KeyCode.S))
         {
+            ListText.text = "Items: ";
             buyItem = "small milk(Blue)";
             buymesgText.text = "You bought " + buyItem + " !";
+            Smilks += 1;
             //update list text
-            ListText.text = buyItem;
+
+            SmilkText.text = buyItem + " " + Smilks;
             //play animation
             BuymesgAnim.SetTrigger("buyUp");
             Invoke("buyReminderOff", 3.5f);
